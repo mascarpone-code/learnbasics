a3cf3d7a98fe5c91c29f7f2505eb0a277d82b638
mascarpone-code <orloff.evgeniy@gmail.com>
2023-11-15 07:43:34 +0700

    add tasks from lecture #3

---
+++ src/_03/MaxMin.java
@@ -0,0 +1,26 @@
+package _03;
+
+import java.util.Scanner;
+
+public class MaxMin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое целое число: ");
+        int number1 = scanner.nextInt();
+
+        System.out.print("Введите второе целое число: ");
+        int number2 = scanner.nextInt();
+
+        if (number1 > number2) {
+            System.out.println("Наибольшее число: " + number1 + ". Наименьшее: " + number2);
+        } else {
+            System.out.println("Наибольшее число: " + number2 + ". Наименьшее: " + number1);
+        }
+
+        int max = (number1 > number2) ? number1 : number2;
+        int min = (number1 < number2) ? number1 : number2;
+
+        System.out.println("Наибольшее число: " + max + ". Наименьшее: " + min);
+    }
+}
---
+++ src/_03/Password.java
@@ -0,0 +1,26 @@
+package _03;
+
+import java.util.Scanner;
+
+public class Password {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите пароль: ");
+        String userInput = scanner.nextLine();
+
+        String password = "PassWord";
+        int passwordLength = password.length();
+        int userInputLength = userInput.length();
+
+        if (userInput.equals(password)) {
+            System.out.println("Пароль верный.");
+        } else if (userInputLength > passwordLength) {
+            System.out.println("Пароль неверный, строка слишком длинная.");
+        } else if (userInputLength < passwordLength) {
+            System.out.println("Пароль неверный, строка слишком короткая.");
+        } else {
+            System.out.println("Пароль неверный");
+        }
+    }
+}
